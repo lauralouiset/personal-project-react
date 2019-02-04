c34c660d6abe07a48813fa64a69012c2b0aca572
Laura-Louise <laura-louise@Laura-Louises-iMac.local>
2019-02-04 14:13:25 -0500

    Beginning

---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Profile extends Component{
+
+
+render(){
+  return(
+      <div className="profile">
+        <img src="" alt=""/>
+      </div>
+  )
+}
+
+}
+
+export default Profile;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchForm extends React.Component{
+
+  render(){
+    return(
+      <form>
+        <input type="text"/>
+      </form>
+    )
+  }
+}
+
+export default SearchForm;
