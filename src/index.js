80e14771525e7d0990d981cc2ee1723ffc2a7813
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2019-02-10 10:32:55 -0500

    beginning redux implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
 
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
